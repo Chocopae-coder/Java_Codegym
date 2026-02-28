d86cf7915442f8fdfc0f2fa7fe2bfa31fa2bd025
Chocopae <doxuantruong2111@gmail.com>
2026-03-01 06:01:54 +0700

    them file

---
+++ CreateRandom/Main.java
@@ -0,0 +1,18 @@
+package CreateRandom;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+	create_arr arrExample = new create_arr();
+	Integer[] arr = arrExample.createRandom();
+		
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("\nVui lòng nhập chỉ số của một phần tử bất kỳ: ");
+	int x = scanner.nextInt();
+	try {
+		System.out.println("Giá trị của phần tử có chỉ số " + x + " là " + arr[x]);
+	} catch (IndexOutOfBoundsException e) {
+		System.err.println("Chỉ số vượt quá giới hạn của mảng");
+	}
+    scanner.close();
+    }
+}
